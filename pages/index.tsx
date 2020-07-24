e9bff6631aeb1e3af263cbccf8c542f6e2c12b90
atrincas <atrincas@gmail.com>
2020-07-24 18:50:56 +0200

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Adam Trincas</title>
+      </Head>
+
+      <main>
+        <h1>Adam Trincas</h1>
+      </main>
+    </>
+  )
+}
