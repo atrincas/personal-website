a5cbee9ccb079c19681c3b60f8e7324b80853b97
atrincas <atrincas@gmail.com>
2020-07-25 08:39:51 +0200

    Added google fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,48 @@
 import Head from 'next/head'
 
+// TODO: Replace this data to database:
+const technologies = [
+  'HTML',
+  'CSS',
+  'Javascript',
+  'React',
+  'Redux',
+  'Styled Components',
+  'Tailwindcss'
+]
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Adam Trincas</title>
+        <link rel="preload" href="/fonts/Ubuntu-Regular.ttf" as="font" crossOrigin="" />
+        <link rel="preload" href="/fonts/AnonymousPro-Bold.ttf" as="font" crossOrigin="" />
       </Head>
 
-      <main>
-        <h1 className="text-red-800">Adam Trincas</h1>
+      <main role="main" className="container-md max-w-screen-md mx-auto px-5 py-10">
+        <header>
+          <hgroup>
+            <h1>Hi, I'm Adam</h1>
+            <h2>Front_End_Developer</h2>
+          </hgroup>
+        </header>
+        <article className="my-5">
+          <h3>Technologies I work with</h3>
+          <ul className="flex flex-wrap">
+            {technologies.map((tech) => (
+              <li
+                key={tech}
+                className="bg-transparent hover:bg-orange-500 text-orange-700 font-semibold hover:text-white text-center m-2 p-2 border border-orange-500 hover:border-transparent rounded"
+              >
+                {tech}
+              </li>
+            ))}
+          </ul>
+        </article>
+        <article className="my-5">
+          <h3>Latest Articles</h3>
+        </article>
       </main>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        heading: ['Anonymous Pro', 'Ubuntu', 'Roboto', 'sans-serif'],
+        ubuntu: ['Ubuntu', 'Roboto', 'sans-serif']
+      },
+      colors: {
+        'light-red': '#FAFAFA',
+        'dark-blue': '#225CE8'
+      },
+      minHeight: {
+        '3': '3rem'
+      }
+    }
   },
   variants: {},
   plugins: []
