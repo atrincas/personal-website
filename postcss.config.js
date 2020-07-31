8c62c0cc72954ba59ddeda49abf50ee41f29ec47
atrincas <atrincas@gmail.com>
2020-07-31 22:26:54 +0200

    Fixed typo

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ let environment = {
   plugins: ['tailwindcss', 'postcss-preset-env']
 }
 
-if (process.env.RAILS_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   environment.plugins.push('@fullhuman/postcss-purgecss', {
     content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
     defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || []
