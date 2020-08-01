e399a48eda5e39ed3844b290cb5ce1d0a3cb3021
atrincas <atrincas@gmail.com>
2020-08-01 14:49:20 +0200

    Added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
+import { useEffect } from 'react'
 import { AppProps } from 'next/app'
+import Router from 'next/router'
+
+import * as gtag from '../lib/gtag'
 import Layout from '../components/Layout'
 import '../styles/index.css'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (process.env.NODE_ENV !== 'production') return
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
   return (
     <Layout>
       <Component {...pageProps} />
